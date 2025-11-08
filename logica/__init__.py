4faf846dcf7cb76aef142ecad51965da0f8f48e8
Dieqoloco <lordmaster511@gmail.com>
2025-11-08 11:25:01 -0300

    Add carpetas_en function and update __all__ in __init__.py

--- logica/__init__.py
+++ logica/__init__.py
@@ -1,5 +1,7 @@
 from .guardar import crear_carpeta
+from .cargar import carpetas_en
 
 __all__ = [
-    "crear_carpeta"
+    "crear_carpeta",
+    "carpetas_en"
 ]
--- logica/cargar.py
+++ logica/cargar.py
@@ -1,1 +1,11 @@
 import tkinter as tk
+import os 
+
+def carpetas_en() -> list:
+    carpetas = []
+    ruta = "simulaciones"
+    for carpeta in os.listdir(ruta):
+        if os.path.isdir(os.path.join(ruta, carpeta)):
+            carpetas.append(carpeta)
+    
+    return carpetas
